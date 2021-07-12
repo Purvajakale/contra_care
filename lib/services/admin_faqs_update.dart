5f6c669d168e80be908f43711e42e50d043f1975
AniketKharpatil <aniketkharpatil@gmail.com>
2021-07-12 14:35:52 +0530

    admin ui update

--- lib/services/admin_faqs_update.dart
+++ lib/services/admin_faqs_update.dart
@@ -1,5 +1,6 @@
 import 'package:contra_care/services/faq1update.dart';
 import 'package:contra_care/services/faq2update.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -26,35 +27,60 @@ class _FaqUpdateState extends State<FaqUpdate> {
       
     });
   }
-
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FAQ data'),
+        title: const Text('FAQ Data',),backgroundColor: Color(0xff7c83fd),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        animationDuration: Duration(milliseconds: 1000),
+        color: Color(0xff7c83fd),
+        animationCurve: Curves.easeInOutQuad,
+        height: 70,
+        buttonBackgroundColor: Colors.blueGrey.shade100,
+        items: <Widget>[
+           Icon(Icons.medical_services_rounded),
+          Icon(Icons.medication_rounded),
+        ],
+        onTap: _onItemTapped,
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'daily use pill',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'e-pill',
-          ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.school),
-          //   label: 'School',
-          // ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
     );
   }
 }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('FAQ data'),
+//       ),
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'daily use pill',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.business),
+//             label: 'e-pill',
+//           ),
+//           // BottomNavigationBarItem(
+//           //   icon: Icon(Icons.school),
+//           //   label: 'School',
+//           // ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         selectedItemColor: Colors.amber[800],
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
--- lib/services/admin_pills_update.dart
+++ lib/services/admin_pills_update.dart
@@ -1,5 +1,6 @@
 import 'package:contra_care/services/pills1_update.dart';
 import 'package:contra_care/services/pills2_update.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -26,35 +27,60 @@ class _PillInfoUpdateState extends State<PillInfoUpdate> {
       
     });
   }
-
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pills data'),
+        title: const Text('Pills Update',),backgroundColor: Color(0xff7c83fd),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        animationDuration: Duration(milliseconds: 1000),
+        color: Color(0xff7c83fd),
+        animationCurve: Curves.easeInOutQuad,
+        height: 70,
+        buttonBackgroundColor: Colors.blueGrey.shade100,
+        items: <Widget>[
+           Icon(Icons.medical_services_rounded),
+          Icon(Icons.medication_rounded),
+        ],
+        onTap: _onItemTapped,
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'i-pill',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'e-pill',
-          ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.school),
-          //   label: 'School',
-          // ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
     );
   }
 }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Pills data'),
+//       ),
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'i-pill',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.business),
+//             label: 'e-pill',
+//           ),
+//           // BottomNavigationBarItem(
+//           //   icon: Icon(Icons.school),
+//           //   label: 'School',
+//           // ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         selectedItemColor: Colors.amber[800],
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
